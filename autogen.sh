745d0dc83ce75bc885189ffdb5f0bdfc1fecc851
Wren <drawnwren@gmail.com>
2017-04-23 02:13:17 +0000

    Added makefile and moved resources to abstractdark/

---
+++ autogen.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+test -z "$srcdir" && srcdir=$(dirname "$0")
+test -z "$srcdir" && srcdir=.
+
+cwd=$(pwd)
+cd "$srcdir"
+
+mkdir -p m4
+autoreconf --verbose --force --install || exit $?
+
+cd "$cwd"
+"$srcdir/configure" $@
