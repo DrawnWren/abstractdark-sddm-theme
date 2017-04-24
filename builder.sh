098de98608d0d5b08c4de39eae6b4082c25ba22b
DrawnWren <drawnwren@gmail.com>
2017-04-23 18:17:30 -0700

    Changed PATH mechanic

--- builder.sh
+++ builder.sh
@@ -1,9 +1,5 @@
 set -e
-which cp
-unset PATH
-for p in $buildInputs; do
-    export PATH=$p/bin${PATH:+:}$PATH
-done
-echo path: $PATH 
+export PATH=$PATH:"$coreutils/bin"
+echo $PATH
 mkdir -p $out/theme
 cp -Rv $src/theme $out
